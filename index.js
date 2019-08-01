e81c82cfb8f0ab4241adbed29629a7abf31db2fe
Sarah Bullard <scadmusbullard@gmail.com>
2019-08-01 22:02:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string) {
 var uppercase ="HELLO!";
 var lowercase = "hello!";
 
-if (string === string.toLowerCase()) {
+if (string === string.toLowerCase()); {
   return "I can't hear you!"
-} else if (string === string.toUpperCase()) {
+} else if (string === string.toUpperCase()); {
   return "YES INDEED!"
-} else if (string === "I love you Grandma." {
+} else if (string === "I love you Grandma."); {
   return "I love you, too"
 }
  
