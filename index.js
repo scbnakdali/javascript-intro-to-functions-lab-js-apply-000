a15ae4472e67f2163d17986fe07e9f26ceb0d25e
Sarah Bullard <scadmusbullard@gmail.com>
2019-08-01 22:07:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (string === string.toLowerCase()) {
   return "I can't hear you!"
  } else if (string === string.toUpperCase()) {
   return "YES INDEED!"
- } else if  {
+ } else  {
   return "I love you, too"
  }
 }
